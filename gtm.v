fd1f884ceab58f57fd1fdb6616ab077547dbebc2
Abhishek Shukla <108926031+shivashukla99@users.noreply.github.com>
2026-08-12 21:43:32 +0530

    Add files via upload

---
+++ gtm.v
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12.08.2026 20:33:37
+// Design Name: 
+// Module Name: gtm
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+ 
+module gtm(
+
+/*
+ ----------primitives----------
+ 1 and 
+ 2 not 
+ 3 nand 
+ 4 nor 
+ 5 or 
+ 6 xor 
+ 7 xnor 
+ 8 bufif 1
+ 9 bufif 0
+ 10 not if 1
+ 11 not if 0
+ 
+
+
+   
+    
+    */
+     );
+endmodule
