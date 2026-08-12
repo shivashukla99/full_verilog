3a52e6d3274f626429f5d7c0755f7090e0ca7f87
Abhishek Shukla <108926031+shivashukla99@users.noreply.github.com>
2026-08-12 20:30:41 +0530

    Add files via upload

---
+++ data_types.v
@@ -0,0 +1,58 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12.08.2026 20:10:43
+// Design Name: 
+// Module Name: data_types
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+// ------------------Full Vrilog HDL By Abhishek Shukla---------------------------
+
+ 
+ 
+ /*
+  ------------Data_types -----------
+  1 net
+      -wire
+      - wand
+      - wor 
+  2 Reg
+  3 Integer
+      size 32Bits
+      Default value = x (unknown)
+      signed datatype
+  4 real 
+  5 time
+  6 string 
+  7 float
+  
+  
+  --------------possible values in verilog ----------
+   0- logic 0 or false condition 
+   1- logic 1 or true condition 
+   x - Unknown 
+   z - High impedence or unconnected condtion 
+  
+ 
+ */
+ 
+ module data_types(
+  //wire w1;
+ // wand a;
+ // assign a=1;
+    );
+endmodule
