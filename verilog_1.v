886ce369d1f791107d22faa7250c495d96feb46a
Abhishek Shukla <108926031+shivashukla99@users.noreply.github.com>
2026-08-12 20:08:30 +0530

    Add files via upload

---
+++ verilog_1.v
@@ -0,0 +1,86 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12.08.2026 19:43:26
+// Design Name: 
+// Module Name: verilog_1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+//   By Abhishek Shukla----Full Verilog HDL 
+/*
+Verilog HDL is a Hardware Discription Language 
+EDA - Electronic design Automation 
+
+Verilog ----------Sysnthesisable and Non-sysnthesisable
+
+-----------------Basic Syntax ----------------
+module <identifier>(port_list);
+.
+.
+.
+code
+.
+.
+.
+
+
+
+
+
+*/
+
+/* module B_1(A,B,C,X,Y);
+
+//Style 1 For writting verilog code 
+input A,B,C;
+output X,Y;
+
+endmodule
+
+
+
+//Style 2 For writting verilog code 
+module B_1(input a,b,c, output x,y);
+endmodule
+*/
+
+module B_1(A,B,C,X,Y);
+
+//Style 1 For writting verilog code 
+input A,B,C;
+output X,Y;
+
+endmodule
+
+/*
+ Rules For Identifiers
+ module <identifiers>
+ 
+ 1) we  should not not use number in first position . For ex :- 1fulladder
+ 2)we should not have space in b/w  For ex :- full adder
+ 3) Not use special character
+ 4) not use 
+            * reg
+            * wire
+            * module integer
+            
+            
+
+
+
+
+*/
+
