5f3421a0d8246a80a033fc9b572950dfa71448c2
Abhishek Shukla <108926031+shivashukla99@users.noreply.github.com>
2026-09-05 23:56:17 +0530

    Update behavioral_modeling_in_verilog.v
    Added comments explaining structured procedural blocks in Verilog.

--- behavioral_modeling_in_verilog.v
+++ behavioral_modeling_in_verilog.v
@@ -1,3 +1,14 @@
+
+
+//Structured procedured or sequential blocks
+// 1 always 
+// 2 initial 
+//--------------always----------//
+
+// It is going to execute unit the simulation ends 
+
+// It is synthesisable - means to implement in the hardwares 
+
 module behavioral_modeling;
   reg [3:0];
 
@@ -7,3 +18,5 @@ always @(i1 or i2 or i3)
   <statements>
 end
 endmodule
+
+
