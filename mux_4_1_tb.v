0e7daf4e6dfd5ed76bb816b14a26dec856727e80
Abhishek Shukla <108926031+shivashukla99@users.noreply.github.com>
2026-09-12 12:37:26 +0530

    Update mux_4_1_tb.v

--- mux_4_1_tb.v
+++ mux_4_1_tb.v
@@ -23,16 +23,18 @@
 module mux_4_1_tb(
 
     );
+    
     reg [1:0]s;
     reg [3:0]in;
     wire y;
+    
     integer i;
     
     
     mux_4_1 dut(s, in ,y);
     
     initial
-    beign
+    begin
     {s, in} =0;
     
     end
@@ -53,13 +55,15 @@ module mux_4_1_tb(
     
     //using for loop
     
-    for (i=0; i<64; i=i+1)beign
+    for (i=0; i<64; i=i+1)begin
+    
     #1;
-    (s,in) =i;
+    {s,in} =i;
  end
-    
-    
+ 
     end
+    
+    
     
     
 endmodule
